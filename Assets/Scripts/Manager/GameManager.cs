60665df2de533591aa5ad750dd3d8d44e45f0bd6
Trần Văn Hiếu <tranvanhieuk0@gmail.com>
2024-10-11 14:03:11 +0700

    Update 11/10/2024

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Cinemachine;
-using Unity.VisualScripting;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
