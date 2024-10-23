82c8d624dac24447ad2f8c0fa83bd58384a4bf2b
Trần Văn Hiếu <tranvanhieuk0@gmail.com>
2024-10-23 21:18:24 +0700

    Update 23/10/2024

---
+++ Assets/Scripts/Game/Manager/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private string sceneStart;
+
+    [SerializeField] private GameObject inputIsKeyboard;
+    [SerializeField] private GameObject inputIsGamepad;
+
+    public static GameObject staticInputIsKeyboard;
+    public static GameObject staticInputIsGamepad;
+
+    private void Start()
+    {
+        staticInputIsKeyboard = inputIsKeyboard;
+        staticInputIsGamepad = inputIsGamepad;
+    }
+
+    public void StartGameWithDelay(string nameSceneStart)
+    {
+        sceneStart = nameSceneStart;
+
+        Invoke("StartGame", 0.3f);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(sceneStart);
+    }
+
+    public void QuitGameWithDelay()
+    {
+        Invoke("QuitGame", 0.3f);
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
